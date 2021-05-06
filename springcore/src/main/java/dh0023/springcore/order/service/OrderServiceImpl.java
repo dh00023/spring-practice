538459f89407d4064f33139543e27ffb144007ac
dahye Jeong <wjdekgp1750@naver.com>
2021-05-06 23:48:20 +0900

    의존관계 주입 연습

--- springcore/src/main/java/dh0023/springcore/order/service/OrderServiceImpl.java
+++ springcore/src/main/java/dh0023/springcore/order/service/OrderServiceImpl.java
@@ -13,15 +13,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-
+    // final은 반드시 값이 있어야한다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // 필드 주입
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+
+    // 생성자 의존관계 주입
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    // 수정자 의존관계 주입
+    /*
+    private  MemberRepository memberRepository;
+    private  DiscountPolicy discountPolicy;
+
+  	@Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+  	@Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+     */
+
+    /*
+        일반의존관계 주입
+        @Component
+        public class OrderServiceImpl implements OrderService{
+                private MemberRepository memberRepository;
+            private DiscountPolicy discountPolicy;
+
+            @Autowired
+            public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+                this.memberRepository = memberRepository;
+                this.discountPolicy = discountPolicy;
+            }
+        }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
