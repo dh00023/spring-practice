29bf76d9eb37aecb34dd1a299eec7772d576707d
dahye Jeong <wjdekgp1750@naver.com>
2021-05-24 00:51:42 +0900

    Servelet HttpServletRequest, HttpServletResponse

--- web-servlet/src/main/java/dh0023/webservlet/WebServletApplication.java
+++ web-servlet/src/main/java/dh0023/webservlet/WebServletApplication.java
@@ -2,7 +2,9 @@ package dh0023.webservlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // servlet 자동 등록
 @SpringBootApplication
 public class WebServletApplication {
 
---
+++ web-servlet/src/main/java/dh0023/webservlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,36 @@
+package dh0023.webservlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dh0023.webservlet.basic.HelloData;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "RequestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    // JSON 결과를 파싱해서 사용할 수 있는 자바 객체로 변환하기 위해 Jacson library 사용
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+
+        response.getWriter().write("OK");
+    }
+}
