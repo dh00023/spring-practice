c37244c259f7aa873f4d52cd530662b40e2bf18b
dahye Jeong <wjdekgp1750@naver.com>
2021-11-18 00:06:00 +0900

    ExecutionContext 데이터 저장 및 argument로 Job수행하기

---
+++ batch-practice/src/main/java/io/spring/batch/javagradle/job/BatchConfiguration.java
@@ -0,0 +1,45 @@
+package io.spring.batch.javagradle.job;
+
+import io.spring.batch.javagradle.tasklet.HelloWorld;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    /**
+     * 실제 스프링 배치 Job 생성
+     */
+    @Bean
+    public Job simpleJob() {
+        return this.jobBuilderFactory.get("simpleJob")
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return this.stepBuilderFactory.get("step")
+                .tasklet(helloWorldTasklet())
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloWorldTasklet() {
+        return new HelloWorld();
+    }
+}
