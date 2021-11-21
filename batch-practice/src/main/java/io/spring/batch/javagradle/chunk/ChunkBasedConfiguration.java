dcbfd44635f4be190f5c67492694bf7e1d57f584
dahye Jeong <wjdekgp1750@naver.com>
2021-11-21 23:32:17 +0900

    CompletionPolicy, Step Listener,Step Flow

--- batch-practice/src/main/java/io/spring/batch/javagradle/chunk/ChunkBasedConfiguration.java
+++ batch-practice/src/main/java/io/spring/batch/javagradle/chunk/ChunkBasedConfiguration.java
@@ -1,5 +1,7 @@
 package io.spring.batch.javagradle.chunk;
 
+import io.spring.batch.javagradle.listener.LoggingStepStartStopListener;
+import io.spring.batch.javagradle.policy.RandomChunkSizePolicy;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -7,6 +9,10 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,9 +44,10 @@ public class ChunkBasedConfiguration {
     @Bean
     public Step chunkStep() {
         return this.stepBuilderFactory.get("chunkStep")
-                .<String, String> chunk(1000) // chunk 기반, 커밋간격 10
+                .<String, String> chunk(randomChunkSizePolicy()) // completePolicy 호출
                 .reader(itemReader())
                 .writer(itemWriter())
+                .listener(new LoggingStepStartStopListener())
                 .build();
     }
 
@@ -62,7 +69,43 @@ public class ChunkBasedConfiguration {
             for (String item : items) {
                 System.out.println(">> current item = " + item);
             }
+            System.out.println(">> end itemWriter chunk " + items.size());
         };
+    }
+
+    @Bean
+    public CompletionPolicy simpleCompletionPolicy() {
+        // 처리된 ITEM 개수를 세어, 이 개수가 임계값에 도달하면 chunk 완료로 표시
+        SimpleCompletionPolicy simpleCompletionPolicy = new SimpleCompletionPolicy(1000);
+
+        return simpleCompletionPolicy;
+    }
+
+    @Bean
+    public CompletionPolicy timeoutCompletionPolicy() {
+        TimeoutTerminationPolicy timeoutTerminationPolicy = new TimeoutTerminationPolicy(3);
+        return timeoutTerminationPolicy;
+    }
+
+    @Bean
+    public CompletionPolicy compositeCompletionPolicy() {
+        CompositeCompletionPolicy policy = new CompositeCompletionPolicy();
+
+        // 여러 정책 설정
+        policy.setPolicies(
+                new CompletionPolicy[]{
+                        new TimeoutTerminationPolicy(3),
+                        new SimpleCompletionPolicy(1000)
+                }
+        );
+
+        return policy;
+
+    }
+
+    @Bean
+    public CompletionPolicy randomChunkSizePolicy() {
+        return new RandomChunkSizePolicy();
     }
 }
 
---
+++ batch-practice/src/main/java/io/spring/batch/javagradle/listener/LoggingStepStartStopListener.java
@@ -0,0 +1,21 @@
+package io.spring.batch.javagradle.listener;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.core.annotation.BeforeStep;
+
+public class LoggingStepStartStopListener {
+
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution) {
+        System.out.println(stepExecution.getStepName() + " 시작");
+    }
+
+
+    @AfterStep
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        System.out.println(stepExecution.getStepName() + " 종료");
+        return stepExecution.getExitStatus();
+    }
+}
