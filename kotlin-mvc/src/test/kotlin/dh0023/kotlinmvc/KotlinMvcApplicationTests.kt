ffdda9c86d9e6c4a84f0ef5da66fc97f992010ae
dh00023 <wjdekgp1750@naver.com>
2022-02-07 23:24:39 +0900

    kotlin-mvc project init

---
+++ kotlin-mvc/src/test/kotlin/dh0023/kotlinmvc/KotlinMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package dh0023.kotlinmvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
