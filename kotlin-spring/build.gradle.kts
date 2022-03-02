acfe019ec6c106b6629ee3666ed92a89cd77bdcd
Dahye Jeong <dev.dh0023@gmail.com>
2022-03-02 13:34:33 +0900

    WAS tomcat에서 undertow로 변경

--- kotlin-spring/build.gradle.kts
+++ kotlin-spring/build.gradle.kts
@@ -67,7 +67,11 @@ project(":mvc-tutorial") {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
-        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-web") {
+            exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+        }
+        implementation("org.springframework.boot:spring-boot-starter-undertow")
+
         implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
         implementation("org.springframework.cloud:spring-cloud-starter-config")
         implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
